f80261123f8ecbac9e9227dbe1c565a770c28864
AkshatRaval <akshatraval199@gmail.com>
2025-10-24 10:32:13 +0530

    Added A project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -4,12 +4,12 @@ export const myProjects = [
         title: "Kalyan Edu. Trust Website",
         description: "A modern, responsive website for Kalyan Education Trust, designed to provide information about the trust's initiatives, events, and contact details.",
         subDescription: [
-            "The website features a clean layout with easy navigation, ensuring visitors can quickly access information about the trust's mission, vision, and ongoing projects. It includes dedicated sections for news, events, and a gallery to showcase activities.",
+            "The website features a clean layout with easy navigation, ensuring visitors can quickly access information about the trust's mission, vision, and ongoing projects. It includes dedicated sections for gallery to showcase activities.",
             "Built with ReactJS and styled-components, the site is optimized for both desktop and mobile devices. It also integrates a contact form for inquiries and supports smooth scrolling for a seamless user experience."
         ],
-        href: "https://kalyangcg.in",
-        image: "assets/Projects/kalyanWeb.png",
-        tags: ["HTML/CSS", "JavaScript", "React", "Tailwind CSS", "Node.js", "Express"]
+        href: "https://www.kalyangcg.in",
+        image: "assets/Projects/newKalyanWeb.png",
+        tags: ["ReactJS", "JavaScript", "Tailwind CSS", "Node.js", "Express", "Firebase"]
     },
     {
         id: 2,
@@ -22,6 +22,18 @@ export const myProjects = [
         href: "https://levelupfinance.vercel.app",
         image: "assets/Projects/personalFinanceApp.png",
         tags: ["React", "Tailwind CSS", "Firebase", "JavaScript"]
+    },
+    {
+        id: 3,
+        title: "AI Powered Project Mentor",
+        description: "A modern, responsive web app designed to help students and freshers to make projects with following a roadmap so they don't get confuse",
+        subDescription: [
+            "The app features a sleek dashboard displaying your ongoing projects, interactive roadmaps, and categorized tasks weekwise. Users can give rough description of their idea and some additional features that they want in app, and in seconds app will give a roadmap for that rough idea",
+            "Built with ReactJS, Tailwind CSS, and Firebase, it includes secure authentication, protected routes, and responsive design for seamless use on desktop and mobile. Smooth animations and an intuitive layout ensure an engaging and productive user experience."
+        ],
+        href: "https://ai-project-mentor.vercel.app/",
+        image: "assets/Projects/ProjectMentor.png",
+        tags: ["React", "Tailwind CSS", "Firebase", "Express.js", "Node.js", "JavaScript"]
     }
 
 ]
